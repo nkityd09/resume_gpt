dfd3330444cac3e240bd8eecd305f437f332c347
nkityd09 <ankity@cloudera.com>
2023-07-09 16:08:20 -0700

    Setting App

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,11 @@ st.set_page_config(**PAGE_CONFIG)
 st.title("Ankit's ResumeGPT")
 
 with st.chat_message("user"):
-    st.write("Hello 👋")
+    st.write("""Hi, I'm ResumeGPT, created by Ankit! I can assist you with answering any questions you may have with regards to Ankit's Professional experience \n Some Examples: \n 
+    1. What are the companies Ankit has worked for ? \n
+    2. How many years of experience does Ankit have ? \n
+    3. Where did Ankit complete his Master's from ?
+    """)
 
 prompt = st.chat_input("Say something")
 if prompt:
