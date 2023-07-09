22ffb377eaba28723a27a0d4bbbd9a29952f8a09
nkityd09 <ankity@cloudera.com>
2023-07-09 16:01:45 -0700

    Testing Page Configs

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,13 @@
 import streamlit as st
 import openai
+import langchain
+
+PAGE_CONFIG = {"page_title":"ResumeGPT", 
+               #"page_icon":image, 
+               "layout":"centered", 
+               "initial_sidebar_state":"auto"}
+
+st.set_page_config(**PAGE_CONFIG)
 
 st.title("Ankit's ResumeGPT")
 
