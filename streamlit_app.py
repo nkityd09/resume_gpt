b892e7cec3b012e7c0ec77d79456931c67806484
nkityd09 <ankity@cloudera.com>
2023-07-09 21:39:43 -0700

    Adding Initial Text

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,19 +32,10 @@ pinecone.init(
 )
 index = pinecone.Index('resume')
 vectorstore = Pinecone(index, embed.embed_query, text_field)
-#docsearch = Pinecone.from_texts([t.page_content for t in texts], embeddings, index_name=index_name)
     
 llm = OpenAI(temperature=0, openai_api_key=st.secrets["OPENAI_API_KEY"])
 chain = load_qa_chain(llm, chain_type = "stuff")
 
-
-#query = "What skills does Ankit Yadav have"
-#docs = vectorstore.similarity_search(query)
-
-
-# if "messages" not in st.session_state:
-#     st.session_state["messages"] = [{"role": "assistant", "content": "Hi, I'm ResumeGPT, created by Ankit! I can assist you with answering any questions you may have with regards to Ankit's Professional experience. Some Examples: \n 1. What are the companies Ankit has worked for ? \n 2. How many years of experience does Ankit have ? \n 3. Where did Ankit complete his 
-# Master's from ? "}]
 
 if "messages" not in st.session_state:
     st.session_state["messages"] = [{"role": "assistant", "content": "Hi, I'm ResumeGPT, created by Ankit! I can assist you with answering any questions you may have with regards to Ankit's Professional experience. Some Examples: \n 1. What are the companies Ankit has worked for ? \n 2. How many years of experience does Ankit have ? \n 3. Where did Ankit complete his Master's from ? "}]
@@ -52,16 +43,13 @@ if "messages" not in st.session_state:
 for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
 
-# messages = st.chat_message("assistant")
-# messages.write("Hi, I'm ResumeGPT, created by Ankit! I can assist you with answering any questions you may have with regards to Ankit's Professional experience. Some Examples: \n 1. What are the companies Ankit has worked for ? \n 2. How many years of experience does Ankit have ? \n 3. Where did Ankit complete his Master's from ? ")
-# st.session_state["messages"] = [{"role": "assistant", "content":
-
-if prompt := st.chat_input():
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    st.chat_message("user").write(prompt)
-    docs = vectorstore.similarity_search(prompt)
-    response = chain.run(input_documents=docs, question=prompt)
-    #msg = response.choices[0].message
-    st.session_state.messages.append(response)
-    st.chat_message("assistant").write(response)
+while True:
+    if prompt := st.chat_input():
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        st.chat_message("user").write(prompt)
+        docs = vectorstore.similarity_search(prompt)
+        response = chain.run(input_documents=docs, question=prompt)
+        #msg = response.choices[0].message
+        st.session_state.messages.append(response)
+        st.chat_message("assistant").write(response)
 
