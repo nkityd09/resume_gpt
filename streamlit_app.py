c58370473a9a06a100670569be3f4e7006d690b1
nkityd09 <ankity@cloudera.com>
2023-07-09 21:43:42 -0700

    Resolving Error

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,8 +41,8 @@ if "messages" not in st.session_state:
     st.session_state["messages"] = [{"role": "assistant", "content": "Hi, I'm ResumeGPT, created by Ankit! I can assist you with answering any questions you may have with regards to Ankit's Professional experience. Some Examples: \n 1. What are the companies Ankit has worked for ? \n 2. How many years of experience does Ankit have ? \n 3. Where did Ankit complete his Master's from ? "}]
 
 for msg in st.session_state.messages:
-    st.chat_message(msg["role"]).write(msg["content"])
-
+    with st.chat_message(msg["role"]):
+        st.write(msg["content"])
 
 if prompt := st.chat_input():
     st.session_state.messages.append({"role": "user", "content": prompt})
