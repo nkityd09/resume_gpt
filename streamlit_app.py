b7c1d2acadeaa08f600d4f3f5148376cfc444a25
nkityd09 <ankity@cloudera.com>
2023-09-01 16:59:57 -0700

    Changing to ChatGPT

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,22 +19,22 @@ st.set_page_config(**PAGE_CONFIG)
 
 st.title("ChatGPT")
 
-# model_name = 'text-embedding-ada-002'
-# embed = OpenAIEmbeddings(
-#     model=model_name,
-#     openai_api_key=st.secrets["OPENAI_API_KEY"]
-# )
-# text_field = "text"
+model_name = 'text-embedding-ada-002'
+embed = OpenAIEmbeddings(
+    model=model_name,
+    openai_api_key=st.secrets["OPENAI_API_KEY"]
+)
+text_field = "text"
 
-# pinecone.init(
-#     api_key=st.secrets["PINECONE_API_KEY"],
-#     environment=st.secrets["PINECONE_ENV"]
-# )
-# index = pinecone.Index('resume')
-# vectorstore = Pinecone(index, embed.embed_query, text_field)
+pinecone.init(
+    api_key=st.secrets["PINECONE_API_KEY"],
+    environment=st.secrets["PINECONE_ENV"]
+)
+index = pinecone.Index('resume')
+vectorstore = Pinecone(index, embed.embed_query, text_field)
     
 llm = OpenAI(temperature=0, openai_api_key=st.secrets["OPENAI_API_KEY"])
-#chain = load_qa_chain(llm, chain_type = "stuff")
+chain = load_qa_chain(llm, chain_type = "stuff")
 
 
 if "messages" not in st.session_state:
@@ -47,13 +47,8 @@ for msg in st.session_state.messages:
 if prompt := st.chat_input():
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
-    #docs = vectorstore.similarity_search(prompt)
-    response =  openai.ChatCompletion.create(
-        model="gpt-3.5-turbo-0613",
-        messages=prompt,
-        function_call="auto",  # auto is default, but we'll be explicit
-    )
-    #response = chain.run(input_documents=docs, question=prompt)
+    docs = vectorstore.similarity_search(prompt)
+    response = chain.run(input_documents=docs, question=prompt)
     #msg = response.choices[0].message
     st.session_state.messages.append({"role": "assistant", "content": response})
     st.chat_message("assistant").write(response)
